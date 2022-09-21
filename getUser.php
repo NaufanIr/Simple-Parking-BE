8a0715d6879d2e29cb48f063446e0769cf0c4ace
NaufanIr <naufanirfanda24@gmail.com>
2022-09-21 10:16:35 +0700

    Adding getHomeInfor.php file

--- getUser.php
+++ getUser.php
@@ -3,7 +3,7 @@ require_once('connect.php');
 
 $userID = $_GET['id'];
 
-$querry = "SELECT * FROM user WHERE id = '$userID'";
+$querry = "SELECT id, nama, no_telp, email, password FROM user WHERE id = '$userID'";
 $sql = mysqli_query($db_connect, $querry);
 
 while ($fetchData = $sql->fetch_assoc()) {
